251d420757e642c508eaa6184421a6021bbaa987
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2019-07-06 20:26:54 -0300

    Updating register and login style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.auth')
 
 @section('content')
-    <form action="{{ route('login') }}" method="POST" class="mx-auto my-24 py-8 px-12 w-2/6 border border-side-focus rounded-lg text-center">
+    <form action="{{ route('login') }}" method="POST" class="mx-auto my-20 py-8 px-12 w-2/6 border border-side-focus rounded-lg text-center">
         @csrf
 
-        <img src="{{ asset('images/logo.png') }}" alt="Logo Laratube" class="w-24 mx-auto mb-6">
+        <a href="{{ route('welcome') }}">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo Laratube" class="w-24 mx-auto mb-6">
+        </a>
 
         <h1 class="text-text text-lg mb-2">Login</h1>
 
         <p class="text-text text-sm mb-6">Prosseguir com login no Laratube</p>
 
-        <div class="w-full relative mb-5 text-left">
+        <div class="w-full relative mb-8 text-left">
             <label class="absolute pin-l pin-t {{ $errors->has('email') ? 'text-main' : 'text-link' }} -mt-3 ml-3 bg-default px-1" for="email">E-mail</label>
             <input type="email" name="email" id="email" placeholder="E-mail" class="outline-none w-full py-3 px-2 border-2 {{ $errors->has('email') ? 'border-main focus:border-main' : 'border-side-focus focus:border-link' }} rounded" autofocus>
 
@@ -24,6 +26,8 @@
             <label class="absolute pin-l pin-t text-link -mt-3 ml-3 bg-default px-1" for="password">Senha</label>
             <input type="password" name="password" id="password" placeholder="Senha" class="outline-none w-full py-3 px-2 border-2 border-side-focus focus:border-link rounded">
         </div>
+
+        <input type="hidden" name="remember" id="remember" value="true">
 
         <div class="text-left">
             <a class="text-link font-semibold text-sm" href="#">Esqueceu sua senha?</a>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,53 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <form action="{{ route('register') }}" method="POST" class="mx-auto my-20 py-8 px-12 w-2/6 border border-side-focus rounded-lg text-center">
+        @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+        <a href="{{ route('welcome') }}">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo Laratube" class="w-24 mx-auto mb-6">
+        </a>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+        <h1 class="text-text text-lg mb-2">Login</h1>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+        <p class="text-text text-sm mb-6">Prosseguir com login no Laratube</p>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        <div class="w-full relative mb-8 text-left">
+            <label class="absolute pin-l pin-t {{ $errors->has('name') ? 'text-main' : 'text-link' }} -mt-3 ml-3 bg-default px-1" for="name">Nome</label>
+            <input type="text" name="name" id="name" placeholder="Nome" class="outline-none w-full py-3 px-2 border-2 {{ $errors->has('name') ? 'border-main focus:border-main' : 'border-side-focus focus:border-link' }} rounded" autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            @error('name')
+                <span class="text-xs text-main font-semibold tracking-widest"><i class="mdi mdi-alert-circle mr-1"></i>{{ $message }}</span>
+            @enderror
         </div>
-    </div>
-</div>
+
+        <div class="w-full relative mb-8 text-left">
+            <label class="absolute pin-l pin-t {{ $errors->has('email') ? 'text-main' : 'text-link' }} -mt-3 ml-3 bg-default px-1" for="email">E-mail</label>
+            <input type="email" name="email" id="email" placeholder="E-mail" class="outline-none w-full py-3 px-2 border-2 {{ $errors->has('email') ? 'border-main focus:border-main' : 'border-side-focus focus:border-link' }} rounded">
+
+            @error('email')
+                <span class="text-xs text-main font-semibold tracking-widest"><i class="mdi mdi-alert-circle mr-1"></i>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div class="w-full relative mb-8 text-left">
+            <label class="absolute pin-l pin-t {{ $errors->has('password') ? 'text-main' : 'text-link' }} -mt-3 ml-3 bg-default px-1" for="password">Senha</label>
+            <input type="password" name="password" id="password" placeholder="Senha" class="outline-none w-full py-3 px-2 border-2 border-side-focus focus:border-link  {{ $errors->has('password') ? 'border-main focus:border-main' : 'border-side-focus focus:border-link' }}rounded">
+
+            @error('password')
+                <span class="text-xs text-main font-semibold tracking-widest"><i class="mdi mdi-alert-circle mr-1"></i>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div class="w-full relative mb-8 text-left">
+            <label class="absolute pin-l pin-t text-link -mt-3 ml-3 bg-default px-1" for="password_confirmation">Confirmação de senha</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirmação de senha" class="outline-none w-full py-3 px-2 border-2 border-side-focus focus:border-link rounded">
+        </div>
+
+        <div class="flex items-center justify-between mt-8">
+            <a class="text-link text-sm font-hairline" href="{{ route('login') }}">Fazer login</a>
+
+            <button class="text-default bg-link font-semibold tracking-wide text-sm px-6 py-2 rounded">Cadastrar</button>
+        </div>
+    </form>
 @endsection
