6eb15ba0ee50655cf8e911f1c2dac010fe56ffea
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2019-07-07 23:56:52 -0300

    Add possible route group coments

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
+/**
+ * Pages
+ */
+// History
+// Like
+// trending
+// subscriptions
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Auth::routes();
-
 Route::get('feed/subscriptions', function () {
     return redirect()->route('welcome');
 })->name('subscriptions')->middleware(['auth']);
+
+Auth::routes();
 
 Route::view('watch/{video}', 'videos.show')->name('videos.watch');
 
@@ -17,3 +24,23 @@ Route::view('watch/{video}', 'videos.show')->name('videos.watch');
  */
 Route::post('subscribe/{channel}', 'ChannelSubscriptionsController@store')->name('subscribe')->middleware(['auth']);
 Route::post('unsubscribe/{channel}', 'ChannelSubscriptionsController@destroy')->name('unsubscribe')->middleware(['auth']);
+
+/**
+ * Channels
+ */
+
+/**
+ * Videos
+ */
+
+/**
+ * Comments
+ */
+
+/**
+ * Likes
+ */
+
+/**
+ * Notifications
+ */
